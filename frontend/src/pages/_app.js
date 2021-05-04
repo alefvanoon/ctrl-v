338ea64a750c772b8e5718ddb989fa4839c9f97e
alefvanoon <53198048+alefvanoon@users.noreply.github.com>
2021-05-04 05:37:49 -0700

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -22,8 +22,6 @@ const App = ({ Component, pageProps }) => (
       />
       <link rel="icon" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;700&display=swap"
-            rel="stylesheet" />
       <title>ctrl-v | a modern, open-source pastebin</title>
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-DE1TYY2F24" />
       <script
